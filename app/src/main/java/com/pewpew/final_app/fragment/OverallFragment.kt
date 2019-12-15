b8ae67c40262eb83657091ea201d1b4ad3b4477f
gan <gan131201@gmail.com>
2019-12-15 18:37:40 +0700

    no message

--- app/src/main/java/com/pewpew/final_app/fragment/OverallFragment.kt
+++ app/src/main/java/com/pewpew/final_app/fragment/OverallFragment.kt
@@ -1,6 +1,7 @@
 package com.pewpew.final_app.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
@@ -10,23 +11,201 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.exploration.cpe101.CustomAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 import com.pewpew.final_app.R
+import com.pewpew.final_app.RegisterActivity
+import com.pewpew.final_app.User
+import kotlinx.android.synthetic.main.fragment_history.*
 import kotlinx.android.synthetic.main.fragment_overall.*
 import java.util.ArrayList
+import java.time.LocalDateTime
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.Month
 
 class OverallFragment : Fragment() {
+
 
   /*  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         chartView.lineColor = Color.BLACK
     }*/
 
+    val user = FirebaseAuth.getInstance().currentUser
+    var uid:String = "5403ad4f-ced2-4845-8211-ccd6226cbf24"
+    var boardid:String =""
+
+    var totallogs:Int = 0
+    var totalmonth:Int=0
+    var month1:Int=0
+    var month2:Int=0
+    var month3:Int=0
+    var totalyear:Int=0
+    var year1:Int=0
+    var year2:Int=0
+    var year3:Int=0
+
+    var phoneNumber:String = "0864062057"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        user?.let {
+            uid = user.uid
+        }
+        ///Check If Exist
+
+        val rootView: View = inflater.inflate(R.layout.fragment_overall, container, false)
+
+        val database2 = FirebaseDatabase.getInstance()
+        val myRef2 = database2.getReference("users/users-"+uid)
+
+        myRef2.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                boardid= p0.child("boardid").value.toString()
+//                Log.e("MainActivity",boardid)
+            }
+            override fun onCancelled(p0: DatabaseError) {
+//                Log.w("MainActivity","Error")
+            }
+        })
+
+
+
+        val time = LocalDate.now()
+        val year = time.year.toString().toDouble()
+        val month = time.monthValue.toString().toDouble()
+        val day = time.dayOfMonth.toString().toDouble()
+//        Log.e("MainActivity",year.toString())
+
+
         return inflater.inflate(R.layout.fragment_overall, container, false)
+    }
+
+    fun getNumberThenPhone(){
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("users/users-"+uid)
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                phoneNumber = p0.child("phone").value.toString()
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:"+phoneNumber)
+                startActivity(intent)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+//                Log.e("MainActivity","Get Number ERROR!")
+            }
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // RecyclerView node initialized here
+        val database5 = FirebaseDatabase.getInstance()
+        val myRef5 = database5.getReference("users")
+        var exist:Int = 0
+        myRef5.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                exist=0
+                for (h in p0.children) {
+                    if (h.key.toString() == "users-"+uid) {
+                        exist = 1
+//                        Log.w("MainActivity",h.key.toString())
+//                        Log.e("MainActivity",h.key.toString())
+                    }
+//                    Log.e("MainActivity", h.key.toString())
+                }
+                if(exist==0){
+//                    Log.e("RegisterActivity","Calling Register")
+                    val intent: Intent = Intent(activity, RegisterActivity::class.java)
+                    startActivity(intent)
+                    return
+                }
+            }
+            override fun onCancelled(p0: DatabaseError) {
+//                Log.w("MainActivity","Error")
+            }
+        })
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("logs")
+        myRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                var i:Int=0
+//                Log.d("MainActivity",boardid)
+                for(h in p0.children){
+                    Log.w("MainActivity",h.child("uid").value.toString())
+                    if(h.child("uid").value.toString()==boardid) {
+                        Log.e("Calling",(LocalTime.now().hour.toString().toDouble()-2).toString())
+                        if(h.child("time").child("day").value.toString()==LocalDate.now().dayOfMonth.toString()){
+                            if(h.child("time").child("hour").value.toString().toDouble()>=LocalTime.now().hour.toString().toDouble()-2){
+//                                Log.w("MainActivity","Found Unusual Activity")
+                                getNumberThenPhone()
+                            }
+                        }
+
+                        if(h.child("level").value.toString().toInt()<30){
+                            month1++
+                        }
+                        else if(h.child("level").value.toString().toInt()<50){
+                            month2++
+                        }
+                        else{
+                            month3++
+                        }
+                        totalmonth++
+//                        }
+                        ///Check Yearly
+//                        if(((month*12)+day)-days<365){
+                        if(h.child("level").value.toString().toInt()<30){
+                            year1++
+                        }
+                        else if(h.child("level").value.toString().toInt()<50){
+                            year2++
+                        }
+                        else{
+                            year3++
+                        }
+                        totalyear++
+//                        }
+                        i = i + 1
+                    }
+                }
+                view.findViewById<TextView>(R.id.totalmonth).text = "Monthly Static :   Total "+totalmonth.toString()
+                view.findViewById<TextView>(R.id.totalyear).text = "Yearly Static :   Total "+totalyear.toString()
+                view.findViewById<TextView>(R.id.month1).text = month1.toString()
+                view.findViewById<TextView>(R.id.month2).text = month2.toString()
+                view.findViewById<TextView>(R.id.month3).text = month3.toString()
+                view.findViewById<TextView>(R.id.year1).text = year1.toString()
+                view.findViewById<TextView>(R.id.year2).text = year2.toString()
+                view.findViewById<TextView>(R.id.year3).text = year3.toString()
+
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.w("MainActivity","Error")
+            }
+        })
+        view.findViewById<TextView>(R.id.totalmonth).text = "Monthly Static :   Total "+totalmonth.toString()
+        view.findViewById<TextView>(R.id.totalyear).text = "Yearly Static :   Total "+totalyear.toString()
+        view.findViewById<TextView>(R.id.month1).text = month1.toString()
+        view.findViewById<TextView>(R.id.month2).text = month2.toString()
+        view.findViewById<TextView>(R.id.month3).text = month3.toString()
+        view.findViewById<TextView>(R.id.year1).text = year1.toString()
+        view.findViewById<TextView>(R.id.year2).text = year2.toString()
+        view.findViewById<TextView>(R.id.year3).text = year3.toString()
+
     }
 
 /*
--- app/src/main/java/com/pewpew/final_app/fragment/SettingsFragment.kt
+++ app/src/main/java/com/pewpew/final_app/fragment/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.pewpew.final_app.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -13,20 +14,45 @@ import com.google.firebase.database.FirebaseDatabase
 import android.view.View.OnClickListener;
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.pewpew.final_app.R
+import org.w3c.dom.Text
+import java.time.LocalDate
+import java.time.LocalTime
 
 
-    private lateinit var auth: FirebaseAuth
+private lateinit var auth: FirebaseAuth
 
     private lateinit var googleSignInClient: GoogleSignInClient
 
 class SettingsFragment : Fragment() {
-    val uid = "12344"
+    var uid = "12344"
+    var boardid:String = "12344"
+    var phoneNumber:String="0864062057"
+    val user = FirebaseAuth.getInstance().currentUser
+
+    fun getNumberThenPhone(){
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("users/users-"+uid)
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                phoneNumber = p0.child("phone").value.toString()
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:"+phoneNumber)
+                startActivity(intent)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e("MainActivity","Get Number ERROR!")
+            }
+        })
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,9 +60,47 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        val database2 = FirebaseDatabase.getInstance()
+        val myRef2 = database2.getReference("users/users-"+uid)
+
+        myRef2.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                boardid= p0.child("boardid").value.toString()
+                Log.e("MainActivity",boardid)
+            }
+            override fun onCancelled(p0: DatabaseError) {
+                Log.w("MainActivity","Error")
+            }
+        })
+
+        val database1 = FirebaseDatabase.getInstance()
+        val myRef1 = database1.getReference("logs")
+        myRef1.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                var i: Int = 0
+                for (h in p0.children) {
+                    if(p0.child("uid").value.toString()==boardid) {
+                        if (h.child("time").child("day").value.toString() == LocalDate.now().dayOfMonth.toString()) {
+                            if (h.child("time").child("hour").value.toString().toDouble() >= LocalTime.now().hour.toString().toDouble() - 2) {
+                                Log.w("MainActivity", "Found Unusual Activity")
+                                getNumberThenPhone()
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e("MainActivity","Error!!!!")
+            }
+        })
+
         val rootView: View = inflater.inflate(R.layout.fragment_settings, container, false)
         ///Get Default Value
 
+        user?.let {
+            uid = user.uid
+        }
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("users/users-"+uid)
 
@@ -45,9 +109,13 @@ class SettingsFragment : Fragment() {
 //                val value = p0!!.getValue(String::class.java
                 var phone:String = p0.child("phone").value.toString()
                 var device:String = p0.child("boardid").value.toString()
+                var name:String = p0.child("name").value.toString()
                 rootView.findViewById<EditText>(R.id.phonetxt).setText(phone)
                 rootView.findViewById<EditText>(R.id.devicetxt).setText(device)
+                var username = rootView.findViewById<View>(R.id.username) as TextView
+                username.text = name
             }
+
 
             override fun onCancelled(p0: DatabaseError) {
                 Log.w("MainActivity","Error")
